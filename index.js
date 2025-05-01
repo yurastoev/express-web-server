d5b7c0376640a23718661894a0acbb2b8c5a550a
Yury Stoev <yura.stoev@gmail.com>
2025-05-01 21:56:23 +0300

    Modify server response for the the root

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World from the Express Web Server");
 });
 
 app.listen(3000);
